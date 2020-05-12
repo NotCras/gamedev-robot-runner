84c5439b35506b26fec62912ec5c4c022e45cb83
NotCras <morrowjo@oregonstate.edu>
2020-05-12 13:19:20 -0700

    added more enemy AI. Can now be provoked.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,28 +8,64 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float chaseRange = 5;
-    
+
     private NavMeshAgent navMeshAgent;
     private float distanceToTarget = Mathf.Infinity;
-    
+    private bool isProvoked = false;
+
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
-    
+
     void Update()
     {
-
         distanceToTarget = Vector3.Distance(target.position, transform.position);
 
-        if (distanceToTarget <= chaseRange)
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget(distanceToTarget);
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            ChaseTarget();
+            isProvoked = true;
         }
         else
         {
             navMeshAgent.SetDestination(transform.position);
         }
-        
     }
+
+    
+    private void EngageTarget(float dist)
+    {
+        if (navMeshAgent.stoppingDistance < dist)
+        {
+            ChaseTarget();
+        }
+        else if (navMeshAgent.stoppingDistance >= dist)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        print("I'm attacking you. -Over.");
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, navMeshAgent.stoppingDistance);
+    }
+    
 }
