d75b18b935d34a0bd3e8ba3a1493e6ba65e877a9
NotCras <morrowjo@oregonstate.edu>
2020-05-12 14:07:49 -0700

    implemented raycasting for weapon shooting. Added enemy health and weapon damage.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Camera fpcamera;
+    [SerializeField] private float range = 30f; 
+    
+    
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        bool madehit = Physics.Raycast(fpcamera.transform.position, fpcamera.transform.forward, out hit, range);
+
+        try
+        {
+            if (madehit)
+            {
+                print("I hit " + hit.transform.name);
+            }
+        }
+        catch
+        {
+            print("I didn't hit anything.");
+        }
+
+    }
+}
