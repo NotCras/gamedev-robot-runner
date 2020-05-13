08136fada215973a09ba8511aa0442da069b057d
NotCras <morrowjo@oregonstate.edu>
2020-05-12 21:44:06 -0700

    gun fx: muzzle flash and impact.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,8 @@ public class Weapon : MonoBehaviour
     [SerializeField] private Camera fpcamera;
     [SerializeField] private float range = 30f;
     [SerializeField] private float weaponDamage = 10f;
+    [SerializeField] private ParticleSystem muzzleFlash;
+    [SerializeField] private GameObject impactFlash;
     
     void Update()
     {
@@ -20,15 +22,28 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+        
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
         bool madehit = Physics.Raycast(fpcamera.transform.position, fpcamera.transform.forward, out hit, range);
 
-        //TODO: add some visual hit effect
-
+        
+        
         try
         {
             if (madehit)
             {
+                CreateHitImpact(hit);
                 print("I hit " + hit.transform.name);
             }
         }
@@ -42,7 +57,13 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
-        enemy.takeDamage(weaponDamage);
 
+        enemy.takeDamage(weaponDamage);
+    }
+
+    private void CreateHitImpact(RaycastHit r)
+    {
+        GameObject impact = Instantiate(impactFlash, r.point, Quaternion.LookRotation(r.normal));
+        Destroy(impact, 0.5f);
     }
 }
